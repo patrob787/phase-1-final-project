63f0e9c25e72e18ca6d00470f9c084b935e0dc64
Patrick Roberts <patrob22@gmail.com>
2023-04-03 23:45:14 -0400

    code written to handle ingredient arrays

--- index.js
+++ index.js
@@ -8,17 +8,48 @@ const pleaseBtn = document.getElementById('please');
 let cocktails = [];
 let baseArray = ['Vodka', 'Gin', 'Tequila', 'Whiskey', 'Rum'];
 let ingredientArray = ['Triple Sec', 'Campari', 'Vermouth', 'Bitters', 'Lemon Juice', 'Lime Juice', 'Ginger Beer', 'Sugar', 'Grapefruit', 'Cranberry', 'Pineapple', 'Mint', 'Grenadine', 'Honey', 'Sparkling Wine', 'Tomato Juice'];
+let teqIngredients = [];
+let whiskeyIngredients = [];
+let vodkaIngredients = [];
+let rumIngredients = [];
+let ginIngredients = [];
 
-//Fetch for cocktail files and assignment to a variable
+
+
+//Fetch for cocktail files and manipulate data
 fetch("http://localhost:3000/Cocktails")
 .then(resp => resp.json())
-.then(data => {cocktails = data})
+.then(data => {
+    cocktails = data
+    
+    teqIngredients = grabIngredients('Tequila');
+    vodkaIngredients = grabIngredients('Vodka');
+    rumIngredients = grabIngredients('Rum');
+    ginIngredients = grabIngredients('Gin');
+    whiskeyIngredients = grabIngredients('Whiskey');
+})
 
 startBtn.addEventListener('click', () => {
     renderBaseLiquor(baseArray);
     startBtn.hidden = true;
 })
 
+
+
+//Function Library
+
+//Grabs unique indredients for each base
+function grabIngredients(liquor) {
+    let ingredients = [];
+    let drinks = cocktails.filter(drink => drink.base === liquor);
+    drinks.forEach((drink) => {
+        ingredients.push(drink.ingredient1, drink.ingredient2)
+    })
+    ingredients = new Set(ingredients);
+    return ingredients;
+}
+
+//Render Base Liquor buttons to Well
 function renderBaseLiquor(array) {
     let h3 = document.createElement('h3');
     h3.textContent = 'Pick a Base';
@@ -33,14 +64,14 @@ function renderBaseLiquor(array) {
 
         btn.addEventListener('dragstart', (e) => {
             e.dataTransfer.setData('text/plain', e.target.id);
-            console.log(e.target.id);
             setTimeout(() => {e.target.hidden = true;})
         }, 0)        
 
         well.append(btn);
     })
-};
+}
 
+//Renders ingredient elements to well
 function renderIngredients(array) {
     let h3 = document.createElement('h3');
     h3.textContent = 'Pick 2 Ingredients';
@@ -62,6 +93,7 @@ function renderIngredients(array) {
     })
 }
 
+//Drag and Drop functions
 function dragEnter(e) {
     e.preventDefault();
 };
@@ -83,8 +115,7 @@ shaker.addEventListener('dragover', dragOver);
 shaker.addEventListener('dragleave', dragLeave);
 
 well.addEventListener('drop', (e) => {
-    console.log(e);
-
+    
     const id = e.dataTransfer.getData('text/plain');
     const draggable = document.getElementById(id);
     draggable.hidden = false;
@@ -96,11 +127,15 @@ shaker.addEventListener('drop', (e) => {
     const id = e.dataTransfer.getData('text/plain');
     const draggable = document.getElementById(id);
     draggable.hidden = false;
-    well.innerHTML = "";
-
-    renderIngredients(ingredientArray);
+    
+    if (baseArray.includes(id)) {
+        well.innerHTML = "";
+        renderIngredients(ingredientArray);
+    };
+    
     e.target.appendChild(draggable);
 })
+
 
 
 
